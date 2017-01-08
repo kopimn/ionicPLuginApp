14b2d9692611a24c0e9dbfdb2e963f891eacaf0d
kopinath <kopi.it7@gmail.com>
2017-01-08 12:31:56 +0530

    ionic kitchensink

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,64 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.controller('SampleCtrl',function($scope,$cordovaAppAvailability,$cordovaGeolocation,$cordovaLocalNotification,$cordovaToast,$cordovaLaunchNavigator){
+  $scope.test="hai";
+  $scope.testAppAvailabity=function(){
+    $cordovaAppAvailability.check('com.whatsapp')
+      .then(function() {
+        // is available
+        alert("app available")
+      }, function () {
+        // not available
+        alert("app not available")
+      });
+  }
+
+
+  $scope.getGeoLocation=function(){
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+  $cordovaGeolocation
+    .getCurrentPosition(posOptions)
+    .then(function (position) {
+      $scope.lat  = position.coords.latitude
+      $scope.long = position.coords.longitude
+    }, function(err) {
+      // error
+      alert("sorry try again")
+    });
+  }
+
+$scope.scheduleDelayedNotification = function () {
+      var now = new Date().getTime();
+      var _10SecondsFromNow = new Date(now + 10 * 1000);
+      
+      $cordovaLocalNotification.schedule({
+        id: 1,
+        title: 'My local Notification',
+        text: 'Text here',
+        at: _10SecondsFromNow
+      }).then(function (result) {
+        // ...
+      });
+    };
+
+  $scope.showToast=function(){
+    $cordovaToast.showLongBottom('Here is a message').then(function(success) {
+    // success
+  }, function (error) {
+    // error
+  });
+  }  
+
+  $scope.launchNavigator = function() {
+    var destination = [ 11.024828,77.0083163];
+  var start = [ 11.018219, 77.021418];
+    $cordovaLaunchNavigator.navigate(destination, start).then(function() {
+      console.log("Navigator launched");
+    }, function (err) {
+      console.error(err);
+    });
+  };
 })
